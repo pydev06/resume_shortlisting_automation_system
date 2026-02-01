0300d72fa17130c5928b17a2a60074fc144d513d
pydev06 <correo2imran@gmail.com>
2026-02-01 15:12:09 +0530

    Add resume logo image to the top of the sidebar

--- frontend/app.py
+++ frontend/app.py
@@ -13,6 +13,7 @@ st.set_page_config(
 
 # Sidebar design
 with st.sidebar:
+    st.image("https://img.icons8.com/color/48/resume.png")
     st.title("📄 Resume Shortlisting")
     st.caption("AI-Powered HR Tool v1.0")
     st.markdown("---")
